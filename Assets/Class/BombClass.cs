5267a5e33d0114f4d4c1681e27d407e24f42a9e0
dajver <lfqdth5@gmail.com>
2013-01-08 13:48:39 +0200

    The final version of the game

---
+++ Assets/Class/BombClass.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class BombClass : MonoBehaviour {
+	private float ySpeed = -4f;
+	public GameObject explosionPrefab;
+	public PlayerClass player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( new Vector3(0f, ySpeed*Time.deltaTime, 0f) );
+		if (transform.position.y < -11) {			
+			Destroy(this.gameObject);			
+		}
+	}	
+	
+	void OnTriggerEnter(Collider obj) {
+		if (obj.gameObject.name == "Shark") {
+			//reset shark
+			obj.gameObject.transform.rotation = Quaternion.identity;
+			obj.gameObject.transform.position = new Vector3(20f, -3f, 8f);
+			player.updateScoreBy(+1);
+			Destroy(gameObject);
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		}
+		if (obj.gameObject.name == "ClownFish") {
+			//reset fish
+			obj.gameObject.transform.rotation = Quaternion.identity;
+			obj.gameObject.transform.position = new Vector3(-20f, -1f, 8f);
+			player.updateScoreBy(-1);
+			Destroy(gameObject);
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		}
+		
+	}
+}
---
+++ Assets/Class/PlayerClass.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour {
+	
+	public float speed;
+	public GameObject bombPrefab;
+	public static int score = 0;
+	public GameObject mainCamera;
+	public GameObject gameBackground;
+	public float nextZ = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 20) {
+			//get new speed
+			speed = Random.Range(8f,12f);
+			transform.position = new Vector3( -20f, transform.position.y, transform.position.z );
+		}		
+		transform.Translate(0, 0, speed * Time.deltaTime);
+		
+		if (Input.anyKeyDown) {
+			GameObject bombObject = (GameObject)Instantiate(bombPrefab);
+			bombObject.transform.position = this.gameObject.transform.position;
+			BombClass bomb = (BombClass)bombObject.GetComponent("BombClass");
+			bomb.player = this;
+		}
+		
+		if (nextZ > mainCamera.transform.position.z) {
+			mainCamera.gameObject.transform.Translate( 
+				3* Mathf.Sin(transform.position.z/2 ) * Time.deltaTime, 
+				0, 
+				-Mathf.Sin(transform.position.z /2 ) * Time.deltaTime *0.3f
+			);
+			mainCamera.gameObject.transform.RotateAroundLocal( Vector3.up, Time.deltaTime*0.1f );
+			gameBackground.gameObject.transform.RotateAroundLocal( Vector3.up, Time.deltaTime*0.1f );
+		}
+	}
+	
+	public void updateScoreBy(int deltaScore) {
+		PlayerClass.score += deltaScore;		
+		if (PlayerClass.score>3) {
+			Application.LoadLevel("WinScene");
+		} else if (PlayerClass.score<-3) {
+			Application.LoadLevel("LooseScene");
+		}
+		if (PlayerClass.score>0) {
+			nextZ = PlayerClass.score*2.5f;
+		}
+	}
+	
+	void OnGUI() {
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 20;
+		GUI.Label(new Rect(10,10,100,20), "Score:"+PlayerClass.score, style );	
+	}
+}
